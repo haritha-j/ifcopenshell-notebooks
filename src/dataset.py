fix visualisation scaling and introduce trig values for direction

src/dataset.py
@@ -17,11 +17,13 @@ def parse_tee_properties(element_data):
   #target = [element_data['radius']/1000, element_data['length']/1000]
   scaled_targets = [element_data['radius1']/1000, element_data['length1']/1000, 
                     element_data['radius2']/1000, element_data['length2']/1000]
-  unscaled_targets = [element_data['direction1'][0], element_data['direction1'][1],
-                      element_data['direction1'][2], element_data['direction2'][0], 
-                      element_data['direction2'][1], element_data['direction2'][2],
-                      element_data['position1'][0]/1000, element_data['position1'][1]/1000,
+  unscaled_targets = [element_data['position1'][0]/1000, element_data['position1'][1]/1000,
                       element_data['position1'][2]/1000]
+  for dir in ['direction1', 'direction2']:
+    for i in range(3):
+      unscaled_targets.append(math.sin(element_data[dir][i]))
+      unscaled_targets.append(math.cos(element_data[dir][i]))
+
 
   #target = [element_data['radius']/1000]
   return np.array(scaled_targets), np.array(unscaled_targets)

src/pointnet.py
@@ -114,14 +114,14 @@ def pointnetloss(outputs, labels, m3x3, m64x64, alpha = 0.0001):
     return criterion(outputs, labels) + alpha * (torch.norm(diff3x3)+torch.norm(diff64x64)) / float(bs)
 
 
-def train(model, savepath, targets, train_loader, val_loader=None,  epochs=50, save=True):
+def train(model, savepath, optimizer, criterion, device, targets, train_loader, val_loader=None,  epochs=50, save=True):
     for epoch in range(epochs): 
-        pointnet.train()
+        model.train()
         running_loss = 0.0
         for i, data in enumerate(train_loader, 0):
             inputs, labels = data['pointcloud'].to(device).float(), data['properties'].to(device)
             optimizer.zero_grad()
-            outputs, m3x3, m64x64 = pointnet(inputs.transpose(1,2))
+            outputs, m3x3, m64x64 = model(inputs.transpose(1,2))
 
             loss = pointnetloss(outputs, labels, m3x3, m64x64)
             loss.backward()
@@ -134,7 +134,7 @@ def train(model, savepath, targets, train_loader, val_loader=None,  epochs=50, s
                         (epoch + 1, i + 1, len(train_loader), running_loss / 10))
                     running_loss = 0.0
 
-        pointnet.eval()
+        model.eval()
         total = 0
 
         # validation
@@ -143,7 +143,7 @@ def train(model, savepath, targets, train_loader, val_loader=None,  epochs=50, s
                 total_val_loss = np.zeros(targets)
                 for data in val_loader:
                     inputs, labels = data['pointcloud'].to(device).float(), data['properties'].to(device)
-                    outputs, __, __ = pointnet(inputs.transpose(1,2))
+                    outputs, __, __ = model(inputs.transpose(1,2))
                     total += labels.size(0)
                     for i in range(targets):
                       val_loss = criterion(outputs[:,i], labels[:,i])
@@ -156,4 +156,4 @@ def train(model, savepath, targets, train_loader, val_loader=None,  epochs=50, s
 
         # save the model
         if save:
-            torch.save(pointnet.state_dict(), savepath +"save_"+str(epoch)+".pth")
+            torch.save(model.state_dict(), savepath +"save_"+str(epoch)+".pth")

src/preperation.py
@@ -7,8 +7,7 @@ import torch
 import copy
 
 import open3d as o3d
-import plotly.graph_objects as go
-import plotly.express as px
+
 import scipy.spatial.distance
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -31,56 +30,7 @@ class Normalize(object):
         #print(properties,properties_norm)
 
         return  (norm_pointcloud, properties_norm)
-
-
-def visualize_rotate(data):
-    x_eye, y_eye, z_eye = 1.25, 1.25, 0.8
-    frames=[]
-
-    def rotate_z(x, y, z, theta):
-        w = x+1j*y
-        return np.real(np.exp(1j*theta)*w), np.imag(np.exp(1j*theta)*w), z
-
-    for t in np.arange(0, 10.26, 0.1):
-        xe, ye, ze = rotate_z(x_eye, y_eye, z_eye, -t)
-        frames.append(dict(layout=dict(scene=dict(camera=dict(eye=dict(x=xe, y=ye, z=ze))))))
-    fig = go.Figure(data=data,
-                    layout=go.Layout(
-                        updatemenus=[dict(type='buttons',
-                                    showactive=False,
-                                    y=1,
-                                    x=0.8,
-                                    xanchor='left',
-                                    yanchor='bottom',
-                                    pad=dict(t=45, r=10),
-                                    buttons=[dict(label='Play',
-                                                    method='animate',
-                                                    args=[None, dict(frame=dict(duration=50, redraw=True),
-                                                                    transition=dict(duration=0),
-                                                                    fromcurrent=True,
-                                                                    mode='immediate'
-                                                                    )]
-                                                    )
-                                            ]
-                                    )
-                                ]
-                    ),
-                    frames=frames
-            )
-
-    return fig
-
-
-def pcshow(xs,ys,zs):
-    data=[go.Scatter3d(x=xs, y=ys, z=zs,
-                                   mode='markers')]
-    fig = visualize_rotate(data)
-    fig.update_traces(marker=dict(size=2,
-                      line=dict(width=2,
-                      color='DarkSlateGrey')),
-                      selector=dict(mode='markers'))
-    fig.show()
-    
+  
 
 # trsansform the centerpoint of a cloud to origin
 def center_bbox(cloud):
@@ -120,3 +70,118 @@ class ToTensor(object):
         assert len(pointcloud.shape)==2
 
         return (torch.from_numpy(pointcloud).float(), torch.from_numpy(properties).float())
+
+
+def random_resample_cloud(points, density):
+    indices = np.arange(points.shape[0])
+    if (len(points) > density):
+        sampled_indices = np.random.choice(indices, density, replace=False)
+    else:
+        sampled_indices = np.random.choice(indices, density, replace=True)
+        
+    #print(len(sampled_indices))
+    return(points[sampled_indices])
+        
+
+def save_cloud(points, output_base, name):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    save_path = os.path.join(output_base, name + ".pcd")
+    o3d.io.write_point_cloud(save_path, pcd)
+
+
+def synthetic_dataset(config, sample_size, element_class, output_base, blueprint, start=0):
+    # setup
+    f = open(config, 'r')
+    config_data  = json.load(f)
+    output_dir = os.path.join(output_base, element_class)
+    #os.makedirs(output_dir)
+
+    metadata = {}
+    for i in tqdm(range(start, sample_size+start)):
+        # generate ifc file
+        ifc = setup_ifc_file(blueprint)
+        owner_history = ifc.by_type("IfcOwnerHistory")[0]
+        project = ifc.by_type("IfcProject")[0]
+        context = ifc.by_type("IfcGeometricRepresentationContext")[0]
+        floor = ifc.by_type("IfcBuildingStorey")[0]
+        
+        ifc_info = {"owner_history": owner_history,
+            "project": project,
+           "context": context, 
+           "floor": floor}
+        
+        # generate ifc element
+        if element_class == 'pipe':
+            e = create_pipe(config_data[element_class], ifc, ifc_info)
+        if element_class == 'elbow':
+            e = create_elbow(config_data[element_class], ifc, ifc_info, blueprint)
+        elif element_class == 'tee':
+            e = create_tee(config_data[element_class], ifc, ifc_info, blueprint)
+    
+        metadata[str(i)] = e
+        ifc.write(os.path.join(output_dir, '%d.ifc' % i))
+    
+    with open(os.path.join(output_dir, 'metadata.json'), 'w') as f:
+        json.dump(metadata, f)
+        
+        
+
+def create_merged_dataset(pcd_path, output_base, element_class, num_scans, density, num_views=3, test_split=0.1):
+    # load data
+    metadata_file = os.path.join(output_base, element_class, 'metadata.json')
+    f = open(metadata_file, 'r')
+    metadata = json.load(f)
+    
+    scans = os.listdir(pcd_path)
+    unique_files = set()
+    for sc in scans:
+        element = int(sc.split('_')[0])
+        unique_files.add(element)
+    
+    print(unique_files)
+    # merge multiple views
+    count = 0
+    metadata_new = {}
+    train_clouds = {}
+    test_clouds = {}
+    test_point = int(len(unique_files)*(1-test_split))
+    print(test_point)
+    for k, un in enumerate(unique_files):
+        for i in range(num_scans-num_views):
+            points = []
+            for j in range(num_views):
+                file_path = os.path.join(pcd_path, (str(un) + '_' + str(i+j) + '.pcd'))
+                pcd = o3d.io.read_point_cloud(file_path)
+                points.append(pcd.points)               
+
+            #merged.points = o3d.utility.Vector3dVector(np.vstack(points))
+            merged_points = np.vstack(points)
+            metadata_new[str(count)] = metadata[str(un)]
+            metadata_new[str(count)]['initial_ifc'] = un
+            if k < test_point:
+                train_clouds[str(count)] = merged_points
+            else:
+                test_clouds[str(count)] = merged_points
+                
+            count += 1
+
+    # resample and save_data
+    test_path = os.path.join(output_base, element_class, 'test')
+    train_path = os.path.join(output_base, element_class, 'train')
+    try:
+        os.mkdir(test_path)
+        os.mkdir(train_path)
+    except:
+        pass
+    
+    for k in train_clouds.keys():
+        sampled_points = random_resample_cloud(train_clouds[k], density)
+        save_cloud(sampled_points, train_path, k)
+        
+    for k in test_clouds.keys():
+        sampled_points = random_resample_cloud(test_clouds[k], density)
+        save_cloud(sampled_points, test_path, k)
+
+    with open(os.path.join(output_base, element_class, 'metadata_new.json'), 'w') as f:
+        json.dump(metadata_new, f)

src/visualisation.py
@@ -10,6 +10,9 @@ import open3d as o3d
 from OCC.Core.gp import gp_Pnt
 from utils.JupyterIFCRenderer import JupyterIFCRenderer
 
+import plotly.graph_objects as go
+import plotly.express as px
+
 from src.geometry import get_corner, get_oriented_bbox, sq_distance
 from src.elements import *
 
@@ -17,7 +20,7 @@ from src.elements import *
 # visualize ifc model and point cloud simultaneously
 def vis_ifc_and_cloud(ifc, cloud, colour="#abe000"):
     viewer = JupyterIFCRenderer(ifc, size=(400, 300))
-    gp_pnt_list = [gp_Pnt(k[0], k[1], k[2]) for k in cloud.points]
+    gp_pnt_list = [gp_Pnt(k[0], k[1], k[2]) for k in cloud]
     viewer.DisplayShape(gp_pnt_list, '#abe000')
     return viewer
 
@@ -58,7 +61,14 @@ def visualize_predictions(cloud, element, preds, blueprint):
         pm = {'r1':preds[0], 'l1':preds[1], 'r2':preds[2],'l2':preds[3], 
               'd1':[preds[4], preds[5], preds[6]], 
               'd2':[preds[7], preds[8], preds[9]] }
-        pm['p1'] = [preds[10]*1000, preds[11]*1000, preds[12]*1000]
+        
+        pm['d1'] = [math.atan2(preds[7], preds[8]), 
+        math.atan2(preds[9], preds[10]), 
+        math.atan2(preds[11], preds[12])]        
+        pm['d2'] = [math.atan2(preds[13], preds[14]), 
+        math.atan2(preds[15], preds[16]), 
+        math.atan2(preds[17], preds[18])]
+        pm['p1'] = [preds[4]*1000, preds[5]*1000, preds[6]*1000]
         pm['p2'] = (np.array(pm['p1']) + (np.array(pm['d1']) * np.array(pm['l1']) * 0.5)).tolist()
         print(pm)
         