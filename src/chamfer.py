fix in-place gradient error

src/chamfer.py
@@ -657,7 +657,7 @@ def generate_elbow_cloud_tensor(preds_tensor, return_elbow_edge=False):
 def generate_socket_elbow_cloud_tensor(preds_tensor):
     # read params
     tensor_size = preds_tensor.shape[0]
-    r, x, y, l = torch.clone(preds_tensor[:,0]), preds_tensor[:,1], preds_tensor[:,2], preds_tensor[:,14]
+    r, x, y, l = torch.clone(preds_tensor[:,0]), torch.clone(preds_tensor[:,1]), torch.clone(preds_tensor[:,2]), torch.clone(preds_tensor[:,14])
     d = get_direction_from_trig_tensor(preds_tensor, 8)
     a = torch.atan2(preds_tensor[:,6], preds_tensor[:,7])
     p = torch.clone(torch.transpose(torch.vstack((preds_tensor[:,3], 
@@ -667,24 +667,29 @@ def generate_socket_elbow_cloud_tensor(preds_tensor):
 
     # find start of elbow section 
     p1 = p - (d * l[:, None])
-    elbow_preds_tensor = torch.clone(preds_tensor)
+    elbow_preds_tensor = preds_tensor.clone()
     elbow_preds_tensor[:,3] = p1[:,0]
     elbow_preds_tensor[:,4] = p1[:,1]
     elbow_preds_tensor[:,5] = p1[:,2]
     
     # slightly reduce radius, and modify x and y
-    elbow_preds_tensor[:,0] = elbow_preds_tensor[:,0] * 0.9
+    elbow_preds_tensor_r = elbow_preds_tensor[:,0] * 0.9
     r_old = torch.sqrt(torch.square(x) + torch.square(y))
     c_a, s_a = torch.cos(a), torch.sin(a)
-    scale_factor = torch.div((r_old - r_old*c_a - l*s_a) / (1-c_a), r_old)
-
-    elbow_preds_tensor[:,1] = torch.mul(elbow_preds_tensor[:,1], scale_factor)
-    elbow_preds_tensor[:,2] = torch.mul(elbow_preds_tensor[:,2], scale_factor)
-    elbow_preds_tensor[:,2] = elbow_preds_tensor[:,2] *scale_factor
+    scale_factor = torch.div((r_old - r_old*c_a - l*s_a), (1-c_a)*r_old)
     
-    p2, p2a, p2b = generate_elbow_cloud_tensor(elbow_preds_tensor, return_elbow_edge=True)
+    elbow_preds_tensor_x = torch.mul(elbow_preds_tensor[:,1], scale_factor)
+    elbow_preds_tensor_y = torch.mul(elbow_preds_tensor[:,2], scale_factor)
+    elbow_preds_tensor_new = torch.column_stack((elbow_preds_tensor_r, 
+                                                 elbow_preds_tensor_x, 
+                                                 elbow_preds_tensor_y, 
+                                                 elbow_preds_tensor[:,3:]))
+    # elbow_preds_tensor[:,2] = torch.mul(elbow_preds_tensor[:,2], scale_factor)
+    # elbow_preds_tensor[:,2] = elbow_preds_tensor[:,2] *scale_factor
+    
+    p2, p2a, p2b = generate_elbow_cloud_tensor(elbow_preds_tensor_new, return_elbow_edge=True)
     d2 = F.normalize(p2a - p2b)
-    elbow_points = generate_elbow_cloud_tensor(elbow_preds_tensor)
+    elbow_points = generate_elbow_cloud_tensor(elbow_preds_tensor_new)
 
     # generate socket points for first socket
     # get new coordinate frame of pipe

src/finetune.py
@@ -12,44 +12,6 @@ from src.visualisation import visualize_predictions
 from src.utils import scale_preds
 
 
-# single thread, adam optimiser
-def chamfer_fine_tune_single(n_iter, step_size, preds, cloud, cat, blueprint):
-    print(cloud.shape)
-    cuda = torch.device('cuda')
-    #cloud = cloud.transpose(1,0)
-    preds_copy = copy.deepcopy(preds)
-    scaled_original_preds = scale_preds(preds_copy.tolist(), cat)
-    preds_t = torch.tensor([preds], requires_grad=True, device=cuda)
-    cloud_t = torch.tensor([cloud], device=cuda)
-
-    optimiser = torch.optim.Adam([preds_t], lr=step_size)
-
-    for i in tqdm(range (n_iter)):
-        t1 = time.perf_counter()
-
-        optimiser.zero_grad()
-        chamfer_loss = get_chamfer_loss_tensor(preds_t, cloud_t, cat)
-        t2 = time.perf_counter()
-                # backward pass for computing the gradients of the loss w.r.t to learnable parameters
-        chamfer_loss.backward()
-        optimiser.step()
-        
-        t3 = time.perf_counter()
-        #print("chamf", t2-t1, "grad", t3-t2)
-
-        print(i, "loss", chamfer_loss.item(), "preds", preds_t.detach().cpu().numpy())
-        
-    # visualise
-    modified_preds = preds_t.detach().cpu().numpy()[0].tolist()
-    scaled_preds = scale_preds(modified_preds, cat)
-    v_orignal, _ = visualize_predictions([cloud.transpose(1,0).tolist()], cat, [scaled_original_preds], 
-                                         blueprint, visualize=True)
-    v_modified, _ = visualize_predictions([cloud.transpose(1,0).tolist()], cat, [scaled_preds], 
-                                          blueprint, visualize=True)
-    
-    return [v_orignal,v_modified], chamfer_loss.item()
-
-
 # additional fix for elbows which have thick edges
 def elbow_correction(n_iter, step_size, modified_preds, cloud, blueprint, gen_cloud_mod):
    
@@ -58,7 +20,7 @@ def elbow_correction(n_iter, step_size, modified_preds, cloud, blueprint, gen_cl
     erroneous_clouds = []
     indices = []
     # filter out erreneous predictions by checking if they can be visualised
-    for i, p in enumerate(scaled_preds):
+    for i, p in enumerate(tqdm(scaled_preds)):
         try:
             v_modified, _ = visualize_predictions([None, None, cloud[i].transpose(1,0).tolist()], "elbow", 
                                                   [scaled_preds[i]], blueprint, visualize=True)
@@ -211,3 +173,41 @@ def chamfer_fine_tune(n_iter, step_size, preds, cloud, cat, blueprint, alpha=1.0
             return modified_preds
         else:
             return modified_preds, chamfer_loss, gen_cloud_mod
+
+
+# single thread, adam optimiser
+def chamfer_fine_tune_single(n_iter, step_size, preds, cloud, cat, blueprint):
+    print(cloud.shape)
+    cuda = torch.device('cuda')
+    #cloud = cloud.transpose(1,0)
+    preds_copy = copy.deepcopy(preds)
+    scaled_original_preds = scale_preds(preds_copy.tolist(), cat)
+    preds_t = torch.tensor([preds], requires_grad=True, device=cuda)
+    cloud_t = torch.tensor([cloud], device=cuda)
+
+    optimiser = torch.optim.Adam([preds_t], lr=step_size)
+
+    for i in tqdm(range (n_iter)):
+        t1 = time.perf_counter()
+
+        optimiser.zero_grad()
+        chamfer_loss = get_chamfer_loss_tensor(preds_t, cloud_t, cat)
+        t2 = time.perf_counter()
+                # backward pass for computing the gradients of the loss w.r.t to learnable parameters
+        chamfer_loss.backward()
+        optimiser.step()
+        
+        t3 = time.perf_counter()
+        #print("chamf", t2-t1, "grad", t3-t2)
+
+        print(i, "loss", chamfer_loss.item(), "preds", preds_t.detach().cpu().numpy())
+        
+    # visualise
+    modified_preds = preds_t.detach().cpu().numpy()[0].tolist()
+    scaled_preds = scale_preds(modified_preds, cat)
+    v_orignal, _ = visualize_predictions([cloud.transpose(1,0).tolist()], cat, [scaled_original_preds], 
+                                         blueprint, visualize=True)
+    v_modified, _ = visualize_predictions([cloud.transpose(1,0).tolist()], cat, [scaled_preds], 
+                                          blueprint, visualize=True)
+    
+    return [v_orignal,v_modified], chamfer_loss.item()

src/graph.py
@@ -10,7 +10,7 @@ import dgl
 from dgl.data import DGLDataset
 import torch
 
-from src.geometry import get_dimensions, vector_normalise
+from src.geometry import get_dimensions, vector_normalise, norm_array
 from src.utils import *
 
 from src.cloud import element_to_cloud
@@ -21,8 +21,8 @@ from src.centerline import flange_radius
 def get_tee_features(preds):
     pm = {'r1':preds[0], 'l1':preds[1], 'r2':preds[2],'l2':preds[3]}
     pm['p2'] = np.array([preds[4], preds[5], preds[6]])
-    pm['d1'] = np.array(get_direction_from_trig(preds, 7))      
-    pm['d2'] = np.array(get_direction_from_trig(preds, 13))
+    pm['d1'] = norm_array(np.array(get_direction_from_trig(preds, 7)))   
+    pm['d2'] = norm_array(np.array(get_direction_from_trig(preds, 13)))
     p1 = pm['p2'] - pm['d1'] * pm['l1'] * 0.5
     p2 = pm['p2'] + pm['d1'] * pm['l1'] * 0.5
     p3 = pm['p2'] + pm['d2'] * pm['l2']
@@ -34,7 +34,7 @@ def get_tee_features(preds):
 
 def get_pipe_features(preds):
     r, l = preds[0], preds[1]
-    d = np.array(get_direction_from_trig(preds, 5))
+    d = norm_array(np.array(get_direction_from_trig(preds, 5)))
     p0 = np.array([preds[2], preds[3], preds[4]])
     p1 = p0 - d * l * 0.5
     p2 = p0 + d * l * 0.5
@@ -45,7 +45,7 @@ def get_pipe_features(preds):
 
 def get_flange_features(preds):
     r1, r2, l1, l2 = preds[0], preds[1], preds[2], preds[3]
-    d = np.array(get_direction_from_trig(preds, 7))
+    d = norm_array(np.array(get_direction_from_trig(preds, 7)))
     p0 = np.array([preds[4], preds[5], preds[6]])
     p1 = p0 - d * l1 * 0.5
     p2 = p0 + d * l2 * 0.5
@@ -58,11 +58,11 @@ def get_flange_features(preds):
 def get_elbow_features(preds):
     r = preds[0]
     p1 = np.array([preds[3], preds[4], preds[5]])
-    d1 = np.array(get_direction_from_trig(preds, 8))
+    d1 = norm_array(np.array(get_direction_from_trig(preds, 8)))
     p2, p_extended = generate_elbow_cloud(preds, return_elbow_edge=True)
-    d2 = vector_normalise(p2 - p_extended)
-    print(d1, d2)
-    print("elbow angle", math.degrees(math.atan2(preds[6], preds[7])), "d angle", math.degrees(np.arccos(np.dot(d1, d2))))
+    d2 = norm_array(p2 - p_extended)
+    #print(d1, d2)
+    #print("elbow angle", math.degrees(math.atan2(preds[6], preds[7])), "d angle", math.degrees(np.arccos(np.dot(d1, d2))))
     
     return {'r1':r/1000, 'r2':r/1000, 'r3':0.,
             'p1':p1/1000, 'p2':p2/1000, 'p3':np.array([0.,0.,0.]),
@@ -96,7 +96,6 @@ def get_features_from_params(path, dataset):
                     original_pred = bp_tee_correction(original_pred, class_metadata[str(ids[i])], cl)
                     params = get_tee_features(original_pred)
                 elif cl == 'elbow' or cl == 'bend':
-                    print(cl)
                     params = get_elbow_features(original_pred)
                 elif cl == 'flange':
                     params = get_flange_features(original_pred)