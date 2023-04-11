fix direction normalisation

src/chamfer.py
@@ -15,7 +15,7 @@ from src.geometry import vector_normalise, vector_mag
 def generate_elbow_cloud(preds, return_elbow_edge=False):
     # read params
     r, x, y = preds[0], preds[1], preds[2], 
-    d = get_direction_from_trig(preds, 8)
+    d = vector_normalise(get_direction_from_trig(preds, 8))
     a = math.atan2(preds[6], preds[7])
     p = [preds[3], preds[4], preds[5]]
     
@@ -54,7 +54,9 @@ def generate_elbow_cloud(preds, return_elbow_edge=False):
     
     if return_elbow_edge:
         delta = 0.01
-        return ((transformed_center + (r_axis * math.cos(a-delta) * b_axis) 
+        return ((transformed_center + (r_axis * math.cos(a) * b_axis) 
+                - (r_axis * math.sin(a) * np.array(d))), 
+                (transformed_center + (r_axis * math.cos(a-delta) * b_axis) 
                 - (r_axis * math.sin(a-delta) * np.array(d))), 
                 (transformed_center + (r_axis * math.cos(a+delta) * b_axis) 
                 - (r_axis * math.sin(a+delta) * np.array(d))))
@@ -116,7 +118,7 @@ def get_cylinder_points(no_of_axis_points, no_of_ring_points, r, l, p, d, x_axis
 def generate_flange_cloud(preds, disc=True):
     # read params
     r1, r2, l1, l2 = preds[0], preds[1], preds[2], preds[3]
-    d = get_direction_from_trig(preds, 7)
+    d = vector_normalise(get_direction_from_trig(preds, 7))
     #d = [preds[7], preds[8], preds[9]]
     p0 = [preds[3], preds[5], preds[6]]
     p = [p0[i] - ((l1*d[i])) for i in range(3)]
@@ -164,7 +166,7 @@ def generate_flange_cloud(preds, disc=True):
 def generate_pipe_cloud(preds, scale=False):
     # read params
     r, l = preds[0], preds[1]
-    d = get_direction_from_trig(preds, 5)
+    d = vector_normalise(get_direction_from_trig(preds, 5))
     p0 = [preds[2], preds[3], preds[4]]
     p = [p0[i] - ((l*d[i])/2) for i in range(3)]
     
@@ -192,8 +194,8 @@ def generate_pipe_cloud(preds, scale=False):
 def generate_tee_cloud(preds, refine=True):
     # read params
     r1, l1, r2, l2 = preds[0], preds[1], preds[2], preds[3]
-    d1 = get_direction_from_trig(preds, 7)
-    d2 = get_direction_from_trig(preds, 13)
+    d1 = vector_normalise(get_direction_from_trig(preds, 7))
+    d2 = vector_normalise(get_direction_from_trig(preds, 13))
     p2 = [preds[4], preds[5], preds[6]]
     p1 = [p2[i] - ((l1*d1[i])/2) for i in range(3)]
     
@@ -266,134 +268,6 @@ def get_direction_from_trig_tensor(preds_tensor, k):
                            0, 1))
 
 
-
-    
-# generate points on surface of elbow
-def generate_elbow_cloud_tensor(preds_tensor, return_elbow_edge=False):
-    #t1 = time.perf_counter()
-    # read params
-    tensor_size = preds_tensor.shape[0]
-    r, x, y = preds_tensor[:,0], preds_tensor[:,1], preds_tensor[:,2]
-    d = get_direction_from_trig_tensor(preds_tensor, 8)
-    a = torch.atan2(preds_tensor[:,6], preds_tensor[:,7])
-    p = torch.transpose(torch.vstack((preds_tensor[:,3], 
-                                      preds_tensor[:,4], 
-                                      preds_tensor[:,5])), 
-                        0, 1)
-
-    # get new coordinate frame of elbow
-    old_z = torch.tensor((0., 0., 1.))
-    old_z = old_z.repeat(tensor_size, 1).cuda()
-    x_axis = F.normalize(torch.cross(d, old_z))
-    y_axis = F.normalize(torch.cross(d, x_axis))
-
-    # compute transformation
-    tensor_0 = torch.zeros(tensor_size).cuda()
-    tensor_1 = torch.ones(tensor_size).cuda()
-    trans_mat = torch.transpose(torch.transpose(torch.stack((torch.column_stack((x_axis, tensor_0)), 
-                                                           torch.column_stack((y_axis, tensor_0)), 
-                                                           torch.column_stack((d, tensor_0)),
-                                                           torch.column_stack((p, tensor_1)))),
-                                              0, 1),
-                              1,2)
-
-    # compute axis
-    theta = torch.atan2(x, y)
-    original_axis_dir = torch.transpose(torch.vstack((torch.cos(theta), 
-                                                      -1*torch.sin(theta), 
-                                                      tensor_0,
-                                                      tensor_0)), 
-                                        0, 1)
-    original_axis_dir = original_axis_dir[:, :, None]
-    transformed_axis_dir = torch.flatten(torch.bmm(trans_mat, 
-                                                   original_axis_dir), 
-                                         start_dim=1)[:, :-1]
-    b_axis = F.normalize(torch.cross(transformed_axis_dir, d))
-
-    # compute parameters
-    original_center =  torch.transpose(torch.vstack((x, 
-                                                     y, 
-                                                     tensor_0,
-                                                     tensor_1)),
-                                       0, 1)
-    original_center = original_center[:, :, None]
-    transformed_center = torch.flatten(torch.bmm(trans_mat, 
-                                                 original_center), 
-                                       start_dim=1)[:, :-1]
-    r_axis = torch.sqrt(torch.square(x) + torch.square(y))
-
-    # sample points in rings along axis
-    no_of_axis_points = 100
-    no_of_ring_points = 20
-    #ring_points = torch.zeros(1,1,1).cuda().expand(tensor_size, no_of_axis_points*no_of_ring_points, 3)
-    #ring_points = torch.zeros((tensor_size, no_of_axis_points*no_of_ring_points, 3)).cuda()
-    ring_points_list = []
-    count = 0
-    
-    if return_elbow_edge:
-        delta = 0.01
-        return (transformed_center + 
-                      ((r_axis * torch.cos(a)).unsqueeze(1) * b_axis) - 
-                      ((r_axis * torch.sin(a)).unsqueeze(1) * d),
-                transformed_center + 
-                      ((r_axis * torch.cos(a-delta)).unsqueeze(1) * b_axis) - 
-                      ((r_axis * torch.sin(a-delta)).unsqueeze(1) * d),
-                transformed_center + 
-                      ((r_axis * torch.cos(a+delta)).unsqueeze(1) * b_axis) - 
-                      ((r_axis * torch.sin(a+delta)).unsqueeze(1) * d))
-
-    #t2 = time.perf_counter()
-
-    # iterate through rings
-    for i in range(no_of_axis_points):
-        
-        # generate a point on the elbow axis
-        angle_axis = (a/no_of_axis_points)*i
-        axis_point = (transformed_center + 
-                      ((r_axis * torch.cos(angle_axis)).unsqueeze(1) * b_axis) - 
-                      ((r_axis * torch.sin(angle_axis)).unsqueeze(1) * d))
-     
-        # find axes of ring plane
-        if (i == 0):
-            ring_x_init = F.normalize(axis_point - transformed_center)
-            ring_y = F.normalize(torch.cross(d, ring_x_init))
-        ring_x = F.normalize(axis_point - transformed_center)
-                
-        # iterate through points in each ring
-        # j = torch.arange(0, no_of_ring_points).cuda()
-        # j = j.unsqueeze(0).repeat(tensor_size, 1)
-        # print("j shape", j.shape)
-
-        # angle_ring = torch.mul(j,2*math.pi/no_of_ring_points)
-        # print("ang ring shape", angle_ring.shape)
-
-        # ring_point = (axis_point + 
-        #                   (torch.mul(r, torch.cos(angle_ring)).unsqueeze(1) * ring_x) - 
-        #                   (torch.mul(r, torch.sin(angle_ring)).unsqueeze(1) * ring_y))
-        # print("ring_point shape", ring_point.shape)
-
-        # ring_points[:,count:count+no_of_ring_points] = ring_point
-        # count += 20
-        for j in range(no_of_ring_points):
-
-            # generate random point on ring around axis point
-            #angle_ring = torch.rand(tensor_size).cuda()*2*math.pi
-            angle_ring = torch.tensor(j*2*math.pi/no_of_ring_points)
-            ring_point = (axis_point + 
-                          ((r * torch.cos(angle_ring)).unsqueeze(1) * ring_x) - 
-                          ((r * torch.sin(angle_ring)).unsqueeze(1) * ring_y))
-            #ring_points[:, count] = ring_point
-            rp = torch.unsqueeze(ring_point, 1)
-            ring_points_list.append(rp)
-            count += 1
-    ring_points = torch.hstack(ring_points_list)
-
-    #t3 = time.perf_counter()
-    #print("cloud", t2-t1, "chamf", t3-t2)
-
-    return(ring_points)
-
-
 # sample points in rings along axis of a cylinder
 def get_cylinder_points_tensor(no_of_axis_points, no_of_ring_points, r, l, p, d, x_axis, y_axis, tensor_size):
     count = 0
@@ -455,7 +329,7 @@ def generate_flange_cloud_tensor(preds_tensor, disc = True):
     # read params
     tensor_size = preds_tensor.shape[0]
     r1, r2, l1, l2 = preds_tensor[:,0], preds_tensor[:,1], preds_tensor[:,2], preds_tensor[:,3]
-    d = get_direction_from_trig_tensor(preds_tensor, 7)
+    d = F.normalize(get_direction_from_trig_tensor(preds_tensor, 7))
     p0 = torch.transpose(torch.vstack((preds_tensor[:,4], 
                                       preds_tensor[:,5], 
                                       preds_tensor[:,6])), 
@@ -504,7 +378,7 @@ def generate_pipe_cloud_tensor(preds_tensor):
     # read params
     tensor_size = preds_tensor.shape[0]
     r, l = preds_tensor[:,0], preds_tensor[:,1]
-    d = get_direction_from_trig_tensor(preds_tensor, 5)
+    d = F.normalize(get_direction_from_trig_tensor(preds_tensor, 5))
     p0 = torch.transpose(torch.vstack((preds_tensor[:,2], 
                                       preds_tensor[:,3], 
                                       preds_tensor[:,4])), 
@@ -533,7 +407,7 @@ def generate_elbow_cloud_tensor(preds_tensor, return_elbow_edge=False):
     # read params
     tensor_size = preds_tensor.shape[0]
     r, x, y = preds_tensor[:,0], preds_tensor[:,1], preds_tensor[:,2]
-    d = get_direction_from_trig_tensor(preds_tensor, 8)
+    d = F.normalize(get_direction_from_trig_tensor(preds_tensor, 8))
     a = torch.atan2(preds_tensor[:,6], preds_tensor[:,7])
     p = torch.transpose(torch.vstack((preds_tensor[:,3], 
                                       preds_tensor[:,4], 
@@ -658,7 +532,7 @@ def generate_socket_elbow_cloud_tensor(preds_tensor):
     # read params
     tensor_size = preds_tensor.shape[0]
     r, x, y, l = torch.clone(preds_tensor[:,0]), torch.clone(preds_tensor[:,1]), torch.clone(preds_tensor[:,2]), torch.clone(preds_tensor[:,14])
-    d = get_direction_from_trig_tensor(preds_tensor, 8)
+    d = F.normalize(get_direction_from_trig_tensor(preds_tensor, 8))
     a = torch.atan2(preds_tensor[:,6], preds_tensor[:,7])
     p = torch.clone(torch.transpose(torch.vstack((preds_tensor[:,3], 
                                       preds_tensor[:,4], 
@@ -725,8 +599,8 @@ def generate_tee_cloud_tensor(preds_tensor, bp=False):
     # read params
     tensor_size = preds_tensor.shape[0]
     r1, l1, r2, l2 = preds_tensor[:,0], preds_tensor[:,1], preds_tensor[:,2], preds_tensor[:,3]
-    d1 = get_direction_from_trig_tensor(preds_tensor, 7)
-    d2 = get_direction_from_trig_tensor(preds_tensor, 13)
+    d1 = F.normalize(get_direction_from_trig_tensor(preds_tensor, 7))
+    d2 = F.normalize(get_direction_from_trig_tensor(preds_tensor, 13))
     p2 = torch.transpose(torch.vstack((preds_tensor[:,4], 
                                       preds_tensor[:,5], 
                                       preds_tensor[:,6])), 

src/graph.py
@@ -1,7 +1,7 @@
 # utilities for graph dataset generation from IFC
 
 from ifcopenshell.util.selector import Selector
-from tqdm import tqdm
+from tqdm import tqdm_notebook as tqdm
 import pickle
 import numpy as np
 
@@ -59,8 +59,8 @@ def get_elbow_features(preds):
     r = preds[0]
     p1 = np.array([preds[3], preds[4], preds[5]])
     d1 = norm_array(np.array(get_direction_from_trig(preds, 8)))
-    p2, p_extended = generate_elbow_cloud(preds, return_elbow_edge=True)
-    d2 = norm_array(p2 - p_extended)
+    p2, p2a,  p2b = generate_elbow_cloud(preds, return_elbow_edge=True)
+    d2 = norm_array(p2b - p2a)
     #print(d1, d2)
     #print("elbow angle", math.degrees(math.atan2(preds[6], preds[7])), "d angle", math.degrees(np.arccos(np.dot(d1, d2))))
     